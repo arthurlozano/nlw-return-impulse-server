8fe255b98d9b1dedbfe1c62d85a41a1df6e60491
Arthur Lozano <athlonlozano@gmail.com>
2022-05-07 11:59:02 -0300

    Adição de limite de urlencoded para 25mb caso necessário (prints de altas resoluções)

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,8 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb', extended: true }));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
